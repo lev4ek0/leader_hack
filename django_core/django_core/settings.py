88fb799e8221729fd4d0283f035a25567532a09d
lev <leva22.08.01@inbox.ru>
2023-05-20 13:29:11 +0300

    feat:add rest

--- django_core/django_core/settings.py
+++ django_core/django_core/settings.py
@@ -186,3 +186,15 @@ CACHES = {
         },
     },
 }
+
+# DRF conf
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+}
