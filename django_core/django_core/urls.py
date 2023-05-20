b93ad29cefaa2789261b81b991226c4ef7287720
lev <leva22.08.01@inbox.ru>
2023-05-20 13:27:11 +0300

    feat:add swagger

--- django_core/django_core/urls.py
+++ django_core/django_core/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -34,5 +35,15 @@ urlpatterns = [
                 ),
             ]
         )
-    )
+    ),
+    path(
+        "schema/",
+        SpectacularAPIView.as_view(),
+        name="schema",
+    ),
+    path(
+        "schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
